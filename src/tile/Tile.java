35620f325941bb35dd112093da2260b5799bc02c
nascvicent <eminhovicente@hotmail.com>
2024-06-29 15:42:05 -0300

    cap fogo

--- src/tile/Tile.java
+++ src/tile/Tile.java
@@ -1,12 +1,14 @@
 package tile;
 
+import main.GamePanel;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class Tile {
 
     // MATERIAIS ESTATICOS
-
+    private static GamePanel gp;
     public static Tile[] tiles = new Tile[100];
     public static Tile pisoTijoloPedra = new PisoTijoloPedra(1);
     public static Tile pisoMadeira = new PisoMadeira(2);
@@ -17,13 +19,13 @@ public class Tile {
     public BufferedImage image;
     protected final int id;
     public boolean colission = false;
-    public static final int tileWidth = 80, tileHeight = 80;
+    public static final int tileWidth = gp.tamanhoJanela, tileHeight = gp.tamanhoJanela;
 
 
     public Tile(BufferedImage image,int id) {
         this.id = id;
         this.image = image;
-
+        this.gp = new GamePanel();
         tiles[id] = this;
     }
 
